3f38d5a65115e44c19c80e1b7cefac32d904df7b
OceaneEmile <oceane.emile@oclock.school>
2023-12-25 22:51:17 +0100

    dynamisation creation boucle dans template home

--- meteo/src/Controller/MainController.php
+++ meteo/src/Controller/MainController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use WeatherModel;
 
 class MainController extends AbstractController
 {
+    
     #[Route('/', name: 'main_home')]
     public function index(): Response
     {
+// Pour dynamiser, je vais instancier WeatherData pour avoir accès au données du tableau
+    $weatherData = WeatherModel::getWeatherData();
+    //dump($weatherData); 
+    //=> OK j'ai bien mon tableau
         return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
+
+            'weatherData' => $weatherData,
+            
         ]);
     }
+     
 }
